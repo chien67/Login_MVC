836fb99affb653b5097811eca6c3696f2285860f
chien67 <95527965+chien67@users.noreply.github.com>
2023-12-22 02:31:08 +0700

    CF
    Login

---
+++ WebApp_Login/WebApp_Login/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebApp_Login.Models;
+
+namespace WebApp_Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        LoginModel db = new LoginModel();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //HttpGet /Home/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //Http Post /Home/Register
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        //HttpGet /Home/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Http Post /Home/Register
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var taikhoanForm = user.TaiKhoan;
+            var matkhauForm = user.MatKhau;
+            var userCheck = db.Users.SingleOrDefault(x=>x.TaiKhoan.Equals(taikhoanForm) && x.MatKhau.Equals(matkhauForm));
+            if(userCheck != null)
+            {
+                Session["User"]= userCheck;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.LoginFail = "Đăng nhập thất bại, Vui lòng thử lại";
+                return View("Login");
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApp_Login/WebApp_Login/Models/User.cs
@@ -0,0 +1,31 @@
+namespace WebApp_Login.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string TaiKhoan { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string MatKhau { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string HoTen { get; set; }
+
+        [StringLength(100)]
+        public string Diachi { get; set; }
+
+        public int? GioiTinh { get; set; }
+    }
+}
